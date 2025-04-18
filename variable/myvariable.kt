1f8e3b8d57712abddd854bfa2f727acdff95b7f1
xaman <xaman282@gmail.com>
2025-04-19 02:12:32 +0530

    variable

---
+++ variable/myvariable.kt
@@ -0,0 +1,53 @@
+package variable
+
+fun main(){
+    //    var name = "Captain" /// var is mutable
+//    name = "Aman kr singh"
+
+
+//    val human = "Aman singh" /// val is immutable we cannot reassign
+//
+//    println("hello $name")
+//    println (human)
+//
+//    var a : Int ///another way to initialize and declaration
+//    a = 20
+//    println(a)
+//
+//    val b: String= "Rahul"
+//    println(b)
+
+
+    ////now lets see different Integer types data///
+    /****
+     * Int 32 bits
+     *  Byte 8 bits
+     *  Short 16 bits
+     *  Long 64 bits
+     */
+
+
+
+    var Num : Int = 2147483647 // max value,, min is -2147483648
+    var Num1 : Byte = 127    // max  , min is -128
+    var Num2 : Short =  32767 // max , min is -32768
+    var num3: Long = 9223372036854775807L // max ,, min is -9223372036854775807L - 1L
+
+
+    /***
+     * Floating Types Data
+     *
+     * Float holds 32 bits
+     * Double holds 32 bits
+     */
+
+    var num4: Float = 3.4028235E38F // max ,, min is 1.4E-45F
+    var num5: Double = 1.7976931348623157E308 //max ,, min is  4.9E-324
+
+
+    var grade: Char = 'A' // you put number also but only one number or letter
+    var wannaCoffee: Boolean = true
+    println("Aman got grade $grade in Android dev, Can i have a coffee? $wannaCoffee")
+
+
+}
