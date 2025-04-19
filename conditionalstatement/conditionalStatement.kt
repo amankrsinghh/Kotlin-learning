949ffe4f5adafc4f0c51c426eaafd679745bdbe0
xaman <xaman282@gmail.com>
2025-04-19 16:39:07 +0530

    operator

---
+++ conditionalstatement/conditionalStatement.kt
@@ -0,0 +1,11 @@
+package conditionalstatement
+
+fun main() {
+    var a = 20
+    var b = 30
+    if(a>b){
+        println( "a is bigger than b")
+    }else{
+        println("B is bigger than a ")
+    }
+}
---
+++ operator/Operator.kt
@@ -0,0 +1,92 @@
+package operator
+
+fun main() {
+
+    /***
+     * 1. Arithmetic operator +, -, *, /, %
+     * Concatenation of String
+     * 2. Assignment Operator =, +=, -=, *=, /=, %=
+     * 3. Increment/Decrement Operator ++, --
+     * 4. Comparison and Equality Operator >, <, >=, <=, ==, !=
+     * 5. Logical Operator ||, &&
+     * */
+
+    /// Addition
+
+//    var a = 20
+//    var b = 30
+//    var sum = a+b
+//    println(sum)
+
+
+    ///subtraction
+//    var a = 20
+//    var b = 30
+//    var sub = a-b
+//    println(sub)
+
+    ///Division
+//    val a = 12.0
+//    val b = 5.0
+//    val div: Float = (a/b).toFloat() // .toFloat() is used for type conversion
+//    println(div)
+
+
+    ///Modulo
+//    val a = 18
+//    val b = 5
+//    val mod = a%b
+//    println(mod)
+
+/// Concatenation
+//    val name = "Aman"
+//    val title = "Singh"
+//    val concat = ("$name $title")
+//    val concat2 = (name+" "+title)
+//    println(concat)
+//    println(concat2)
+
+
+
+    ////Assignment Operator
+//    var a = 20
+//    var b = 40
+//    a+=b   // a = a+b
+//    println(a)
+//
+
+//    var a = 20
+//    var b = 40
+//    a/=b // a = a+b
+//    println(a)
+
+//    var a = 20
+//    var b = 40
+//    a-=b   // a = a+b
+//    println(a)
+
+//    var a = 85.0
+//    var b = 40.0
+//    a/=b  // a = a+b
+//    println(a)
+
+//    var a = 80
+//    var b = 40
+//    a%=b   // a = a+b
+//    println(a)
+
+
+    ///// Increment And Decrement Operator ///
+//    var a = 20
+//    a++
+//    println(a)
+
+//    var a = 20
+//    println(a++)  // pre - increment the value get updated in the memory only
+//    println(a) // now by this we gte the updated value
+//
+
+//    var a = 30
+//    print(++a)// this is post-increment
+
+}
