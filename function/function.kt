499e7cb1e1bfc5e96e5196664c8a67a028e614a0
xaman <xaman282@gmail.com>
2025-04-24 13:57:39 +0530

    function

---
+++ function/function.kt
@@ -0,0 +1,51 @@
+package function
+
+fun main() {
+/*** call fun task() which has no return type and no argument ***/
+    //    task()
+
+ /*** fun task2() in this way you can call return type function ***/
+    //    println(task2())
+  /*** fun task2() in this way also by storing in var then print ***/
+    //    var a = task2()
+    //    println(a)
+
+    /*** fun task3() with argument, no return type ***/
+//    task3(2,5)
+
+    /*** fun task3() with argument and return type ***/
+//    println(task4(23,56))
+
+
+    /*** Lambda function ***/
+//    var tasklambda = {a: Int,b: Int -> a+b}
+//     println(tasklambda(2,3))
+}
+
+/*** this function has no argument and not any return type  ***/
+//fun task(){
+//    var a = 2
+//    var b = 3
+//    var c = a+b
+//    println("The sum of $a and $b is $c")
+//}
+
+/*** this function is with return type ***/
+//fun task2(): Int{
+//    var a = 2
+//    var b = 3
+//    var c = a+b
+//    return c
+//}
+
+/*** function with argument ***/
+//fun task3( a: Int, b: Int){
+//    var c = a+b
+//    println(c)
+//}
+
+/*** function with argyment and return type ***/
+//fun task4(a:Int,b:Int): Int {
+//    var c = a + b
+//    return c
+//}
