f2ba3794bab005697096385b8c6c729fa472fad1
xaman <xaman282@gmail.com>
2025-04-27 15:39:10 +0530

    abstraction

---
+++ oops/abstraction.kt
@@ -0,0 +1,19 @@
+package oops
+
+abstract class Atm {
+    abstract fun process()
+    fun process2(){
+        var res = 20000
+        println("Total Bank Balance:- $res")
+    }
+}
+
+class User: Atm() {
+    override fun process() {
+        process2()
+    }
+}
+fun main() {
+    var obj = User()
+    obj.process()
+}
